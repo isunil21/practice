3f60c3dd1dd641447591c121c89e1719606a20c2
Sunil Chaurasiya <76986471+isunil21@users.noreply.github.com>
2021-01-14 11:43:40 +0545

    Update settings.py

--- best/settings.py
+++ best/settings.py
@@ -122,5 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = ['best/static']
-STATIC_ROOT = ('aid')
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR, 'best/static')
+]
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
+
